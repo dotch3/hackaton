45f9a76ae18028d135fa5acb36bc91db48b5f8f8
Henrique Góes <henriquegb.goes@gmail.com>
2020-08-21 04:23:33 -0300

    Calcular Periodo de Publicacao

--- items/src/main/java/com/startupone/boavizinhanca/items/service/ItemService.java
+++ items/src/main/java/com/startupone/boavizinhanca/items/service/ItemService.java
@@ -1,8 +1,9 @@
 package com.startupone.boavizinhanca.items.service;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,24 @@ public class ItemService {
 	}
 	
 	public List<Item> getItems(){
-		return repository.findAll();
+		List<Item> items = repository.findAll();
+		calcularPeriodoPublicacao(items);
+		return items;
+	}
+
+	private void calcularPeriodoPublicacao(List<Item> items) {
+		LocalDate dateHoje = LocalDate.now();
+		for(Item item : items) {
+			if(item.getDataPublicacao() != null && (item.getDataPublicacao().length() == 10)) {				
+				String dataPublicacao = item.getDataPublicacao();
+				String ano = dataPublicacao.substring(6);
+				String mes = dataPublicacao.substring(3, 5);
+				String dia = dataPublicacao.substring(0, 2);
+				LocalDate datePublicacao = LocalDate.of(Integer.parseInt(ano), Integer.parseInt(mes), Integer.parseInt(dia));
+				Period periodo = Period.between(datePublicacao, dateHoje);
+				item.setDataPublicacao(periodo.getDays() == 0 ? "Hoje" : ("Há " + periodo.getDays() + (periodo.getDays() == 1 ? " dia" : " dias")));
+			}
+		}
 	}
 	
 	public Item getItemById(int id){
