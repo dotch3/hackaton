e2bcc9f3788b83c53230eca87ec017289e75ac74
jorge mercado <dotch3@gmail.com>
2020-08-23 19:02:25 -0300

    cadastro de item enviando dados e criando JSON

--- items/src/main/java/com/startupone/boavizinhanca/items/controller/ItemController.java
+++ items/src/main/java/com/startupone/boavizinhanca/items/controller/ItemController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.startupone.boavizinhanca.items.entity.Item;
 import com.startupone.boavizinhanca.items.service.ItemService;
 
+//test
+import java.io.PrintWriter;
+
 @RestController
 @RequestMapping("/items") 
 public class ItemController {
@@ -51,4 +54,75 @@ public class ItemController {
 	public String deleteItem(@PathVariable int id) {
 		return service.deleteItem(id);
 	}
+	
+	//Testing for addItem
+	@PostMapping("/testPublish")
+	protected void addItem(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String idUserProprietario = request.getParameter("username");
+		String nome = request.getParameter("password");
+		String quantidadeDisponivel = request.getParameter("quantidadeDisponivel");
+		String valor = request.getParameter("valor");
+		String tipoValor = request.getParameter("tipoValor");
+
+		String descricao = request.getParameter("descricao");
+		String foto = request.getParameter("foto");
+		// Optional
+		String dataSugestaoEmprestimo = request.getParameter("dataSugestaoEmprestimo");
+		String dataSugestaoDevolucao = request.getParameter("dataSugestaoDevolucao");
+
+		// Tags
+
+		String tags[] = request.getParameterValues("tags");
+		String tagsHtml = "";
+
+		if (tags != null) {
+			System.out.println("Tags are: ");
+			for (String tag : tags) {
+				tagsHtml += tag + ",";
+				System.out.println("\t" + tag);
+			}
+		}
+
+		System.out.println("Id User is: " + idUserProprietario);
+		System.out.println("nome is: " + nome);
+		System.out.println("quantidadeDisponivel is: " + quantidadeDisponivel);
+		System.out.println("valor  is: " + valor);
+		System.out.println("tipoValor  is: " + tipoValor);
+		System.out.println("tags  is: " + tagsHtml);
+		System.out.println("descricao  is: " + descricao);
+		System.out.println("foto  is: " + foto);
+		System.out.println("dataSugestaoEmprestimo  is: " + dataSugestaoEmprestimo);
+		System.out.println("dataSugestaoDevolucao  is: " + dataSugestaoDevolucao);
+
+		PrintWriter writer = response.getWriter();
+
+		String htmlRespone = "<html><h3>";
+		htmlRespone += "user ID is: " + idUserProprietario + "<br/>";
+		htmlRespone += "nome is: " + nome + "<br/>";
+		htmlRespone += "quantidadeDisponivel is: " + quantidadeDisponivel + "<br/>";
+		htmlRespone += "valor is: " + valor + "<br/>";
+		htmlRespone += "tipoValor is: " + tipoValor + "<br/>";
+		htmlRespone += "descricao is: " + descricao + "<br/>";
+
+		htmlRespone += "foto base64 is: " + foto + "<br/>";
+		htmlRespone += "<p 'id=b64'></p>";
+		htmlRespone += "<img id='img' height='150'>";
+		htmlRespone += "Sugestao Inicio Emprestimo is: " + dataSugestaoEmprestimo + "<br/>";
+		htmlRespone += "Sugestao Fim Emprestimo is: " + dataSugestaoDevolucao + "<br/>";
+		htmlRespone += "</h3></html>";
+
+		// return response
+		writer.println(htmlRespone);
+	}
+	
+	@GetMapping("/error")
+	public void erroring (){
+		htmlRespone += "<html><h3>Error!!</h3></html>";
+
+		// return response
+		writer.println(htmlRespone);
+		return service.getItems();
+	}
+
 }
--- items/src/main/java/com/startupone/boavizinhanca/items/entity/Item.java
+++ items/src/main/java/com/startupone/boavizinhanca/items/entity/Item.java
@@ -7,8 +7,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
 @Table(name = "tb_item_publicado")
+@Getter
+@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Item {
 	
 	@Id
