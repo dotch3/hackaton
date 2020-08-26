e118fad117e0f655ad2d76c7bcb1ecc4d2967ffa
Matheus Souza <matheus.souza@original.com.br>
2020-08-26 15:51:04 -0300

    Remoção de projeto não utilizado

--- users-java/src/main/java/br/com/boavizinhanca/users/repositories/PartnerRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.boavizinhanca.users.repositories;
-
-import br.com.boavizinhanca.users.models.Partner;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PartnerRepository extends JpaRepository<Partner, String> {
-}
