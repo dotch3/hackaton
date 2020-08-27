d34bb562a49190614bbcdcd2958e3cf1c812d47b
Henrique Góes <henriquegb.goes@gmail.com>
2020-08-27 01:09:28 -0300

    Adicionado o Cross

--- items/src/main/java/com/startupone/boavizinhanca/items/controller/FavoritosController.java
+++ items/src/main/java/com/startupone/boavizinhanca/items/controller/FavoritosController.java
@@ -3,6 +3,7 @@ package com.startupone.boavizinhanca.items.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.startupone.boavizinhanca.items.entity.Favorito;
 import com.startupone.boavizinhanca.items.service.FavoritoService;
 
+@CrossOrigin(maxAge = 3600) 
 @RestController
 @RequestMapping("/favoritos") 
 public class FavoritosController {
@@ -21,21 +23,25 @@ public class FavoritosController {
 	@Autowired
 	private FavoritoService service;
 	
+	@CrossOrigin(origins = {"http://54.163.66.128:8080","http://localhost:8080","http://localhost:8888"},maxAge=4800, allowCredentials="false")
 	@GetMapping("/todos")
 	public List<Favorito> findAllItems(){
 		return service.getFavoritos();
 	}
 	
+	@CrossOrigin(origins = {"http://54.163.66.128:8080","http://localhost:8080","http://localhost:8888"},maxAge=4800, allowCredentials="false")
 	@GetMapping("/porUsuario/{id}")
 	public List<Favorito> findByIdUser(@PathVariable int id){
 		return service.getFavoritosByUser(id);
 	}
 	
+	@CrossOrigin(origins = {"http://54.163.66.128:8080","http://localhost:8080","http://localhost:8888"},maxAge=4800, allowCredentials="false")
 	@PostMapping("/favoritar")
 	public Favorito favoritar(@RequestBody Favorito favorito) {
 		return service.adicionar(favorito);
 	}
 	
+	@CrossOrigin(origins = {"http://54.163.66.128:8080","http://localhost:8080","http://localhost:8888"},maxAge=4800, allowCredentials="false")
 	@DeleteMapping("/remover")
 	public String removerFavorito(@RequestBody Favorito favorito) {
 		return service.remover(favorito);
