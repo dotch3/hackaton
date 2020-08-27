5d6cd39d6bc56d616e2dc7175dc23e96615c59ad
jorge mercado <dotch3@gmail.com>
2020-08-27 18:12:13 -0300

    adding lib to cad-user services

--- cad-users-java/src/main/java/br/com/boavizinhanca/cad/controllers/PartnerController.java
+++ cad-users-java/src/main/java/br/com/boavizinhanca/cad/controllers/PartnerController.java
@@ -12,6 +12,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 @CrossOrigin(origins = {"http://54.163.66.128:8080","http://localhost:8080","http://localhost:8081","http://localhost:8888","http://54.163.66.128:8081"},maxAge=4800, allowCredentials="false")
 @RestController()
 @Api(value = "Partner's API",tags = { "Partners" })
