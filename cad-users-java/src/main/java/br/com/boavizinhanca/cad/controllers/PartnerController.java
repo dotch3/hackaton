65e699c0ac56f35efd76edc6b588e9d31c42ed69
jorge mercado <dotch3@gmail.com>
2020-08-27 22:56:32 -0300

    adding localhost

--- cad-users-java/src/main/java/br/com/boavizinhanca/cad/controllers/PartnerController.java
+++ cad-users-java/src/main/java/br/com/boavizinhanca/cad/controllers/PartnerController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = {"http://54.163.66.128:8080","http://localhost:8080","http://localhost:8081","http://localhost:8888","http://54.163.66.128:8081"},maxAge=4800, allowCredentials="false")
+@CrossOrigin(origins = {"http://54.163.66.128:8080","http://localhost","http://localhost:8080","http://localhost:8081","http://localhost:8888","http://54.163.66.128:8081"},maxAge=4800, allowCredentials="false")
 @RestController()
 @Api(value = "Partner's API",tags = { "Partners" })
 @RequestMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
